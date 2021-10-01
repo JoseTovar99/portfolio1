94c8e170b1dde36f76b050a1bf68abaffad5c3f1
JoseTovar510 <tovarjose1013@gmail.com>
2021-09-30 21:17:56 -0700

    Removed jquery

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-new TypeIt(".first-section .hi", {
-    speed: 50,
-    waitUntilVisible: true,
-  })
-    .type("", { delay: 300 })
-    .go();
-
+//new TypeIt(".first-section .hi", {
+//    speed: 50,
+//    waitUntilVisible: true,
+//  })
+//    .type("", { delay: 300 })
+//    .go();
+//
     
