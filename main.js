12249ff579405358bd83dfe4241702d3c250f497
JoseTovar510 <tovarjose1013@gmail.com>
2021-11-04 18:51:39 -0700

    Updated styling changes

--- main.js
+++ main.js
@@ -11,4 +11,14 @@ const aboutList = document.getElementById("about-list");
 
 hamburger.addEventListener('click', () => {
     aboutList.classList.toggle('show');
-})
+});
+
+
+//const [red, green, blue] = [69, 111, 225]
+//const section1 = document.querySelector('.container2')
+//
+//window.addEventListener('scroll', () => {
+//  const y = 1 + (window.scrollY || window.pageYOffset) / 150
+//  const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+//  section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+//})
